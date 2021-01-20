934b31ede9aa0d4e86c384c8fcb0a0631a4fb144
Chi-Tuvshinbayar <chi.tuvshinbayar@gmail.com>
2021-01-20 21:30:22 +0800

    Finance module hiij ehelsen. Expense, Income object uudiig uusgesen

--- app.js
+++ app.js
@@ -1,30 +1,81 @@
 // Дэлгцэтэй ажиллах контроллер
 var uiController = (function(){
-    console.log('uiController-ын function');
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        addBtn: '.add__btn'
+    };
+    return{
+        getInput: function(){
+            return{
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value                
+            };
+        },
+
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
 })();
 // Санхүүтэй ажиллах контроллер
 var financeController = (function(){
-    console.log('financeController-ын function');
+    // Income, Expense хадгалах байгууллагч функцыг үүсгэнэ.
+    var Income = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;    
+    }
+    var Expense = function(id, description, value){
+        this.id = id;
+        this.description = description;
+        this.value = value;    
+    }
+    var data = {
+        allItems: {
+            inc: [],
+            exp: []
+        },
+        totals: {
+            inc: 0,
+            exp: 0
+        }
+    }
 })();
 // Программын холбогч контроллер
 var appController = (function(uiController, financeController){
+    
     var ctrlAddItem = function(){
         // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
-        console.log('clicked ...');
+        console.log(uiController.getInput());
         // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
         // 3. Олж авсан өгөгдлүүдээ вэб дээр тохирох хэсэгт гаргана.
         // 4. Төсвийг тооцоолно.
         // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэд гаргана.
     }
-    document.querySelector('.add__btn').addEventListener('click', function(){
-        ctrlAddItem();
-    });
-    console.log('appController-ын function');
-
-    document.addEventListener("keypress", function(event){
-        // Дээр үеийн browser-уудад event.which-ыг ашигладаг. Орчин үеийн browser-уудад event.keyCode-ыг ашигладаг
-        if(event.keyCode === 13 || event.which === 13 ) {
+    var setupEventListeners = function(){
+        var DOM = uiController.getDOMstrings();
+        document.querySelector(DOM.addBtn).addEventListener('click', function(){
             ctrlAddItem();
+        });
+        console.log('appController-ын function');
+    
+        document.addEventListener("keypress", function(event){
+            // Дээр үеийн browser-уудад event.which-ыг ашигладаг. Орчин үеийн browser-уудад event.keyCode-ыг ашигладаг
+            if(event.keyCode === 13 || event.which === 13 ) {
+                ctrlAddItem();
+            }
+        });
+    }
+
+    return {
+        init: function(){
+            console.log('Application started ...');
+            setupEventListeners();
         }
-    });
+    }
 })(uiController, financeController);
+
+appController.init();
